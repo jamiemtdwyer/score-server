11086165cd6d2ddda1971715d4067a4f419fd5e6
Jamie Dwyer <jamie@halfhelix.com>
2020-09-04 15:25:19 -0400

    Add computed rank attribute to Score

--- models/score.rb
+++ models/score.rb
@@ -1,4 +1,10 @@
 class Score < ActiveRecord::Base
   validates_presence_of :name
   validates_numericality_of :score, greater_than: 0
+
+  attribute :rank, :integer
+
+  def rank
+    self.class.where("score > ?", score).count + 1
+  end
 end
